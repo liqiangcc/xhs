2c35a5ebfa6170d17159e2e5e5f37f5be00348eb
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-12 16:26:50 +0800

    refactor: wire canonical accept composition root

--- src/bootstrap/create-application.js
+++ src/bootstrap/create-application.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const { createAcceptCanonicalUseCase } = require('../application/canonical/accept-canonical');
 const { createMergeCanonicalUseCase } = require('../application/canonical/merge-canonical');
 const { createSplitCanonicalUseCase } = require('../application/canonical/split-canonical');
 const { loadTaxonomy } = require('../infrastructure/config/taxonomy-provider');
 const { createCanonicalFsPaths } = require('../infrastructure/filesystem/canonical-paths');
 const { createFsCanonicalRepositories } = require('../infrastructure/filesystem/canonical-repositories');
+const { createFsCanonicalCandidateRepository } = require('../infrastructure/filesystem/canonical-candidate-repositories');
 const { createFsReviewRepository } = require('../infrastructure/filesystem/review-repositories');
 const { createFsAnswerRepository } = require('../infrastructure/filesystem/answer-repositories');
 const { createFsCanonicalIntegrityChecker } = require('../infrastructure/filesystem/canonical-integrity-checker');
@@ -25,7 +27,9 @@ function createApplication(options = {}) {
     const {
         canonicalRepository,
         questionBindingRepository,
+        canonicalQuestionOwnershipRepository,
     } = createFsCanonicalRepositories({ root: options.root, paths });
+    const candidateRepository = createFsCanonicalCandidateRepository({ root: options.root, paths });
     const reviewRepository = createFsReviewRepository({ root: options.root, paths });
     const answerRepository = createFsAnswerRepository({ root: options.root, paths });
     const integrityChecker = createFsCanonicalIntegrityChecker({ root: options.root, paths });
@@ -49,11 +53,20 @@ function createApplication(options = {}) {
         mutationStore,
         taxonomy,
     });
+    const accept = createAcceptCanonicalUseCase({
+        candidateRepository,
+        canonicalIdentityRepository: canonicalRepository,
+        canonicalQuestionOwnershipRepository,
+        questionBindingRepository,
+        mutationStore,
+        taxonomy,
+    });
 
     return Object.freeze({
         canonical: Object.freeze({
             merge,
             split,
+            accept,
         }),
     });
 }
